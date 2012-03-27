b125ac092b15d15dda1496d0ff2f03f32e1c93af
Katelyn <katelyn.thode@gmail.com>
2012-03-27 14:06:06 -0500

    Added sensor and LED registers for FPGA logic

--- arm/FreeRTOS/board/tll6219/regaddr.h
+++ arm/FreeRTOS/board/tll6219/regaddr.h
@@ -1,2 +1,13 @@
-#define LEDREG1 0xd3000000
+
+/* Registers for sensors and LEDs in fpga logic */
+#define SENSREGA 0xd3000000
+#define SENSREGB 0xd3000004
+#define SENSREGC 0xd3000008
+#define SENSREGD 0xd300000c
+#define SENSREGE 0xd3000010
+#define SENSREGF 0xd3000014
+#define SENSREGG 0xd3000018
+#define LED1REG  0xd300001c
+#define LED2REG  0xd3000020
+#define LED3REG  0xd3000024
 
