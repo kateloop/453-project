d229eb3454c9c3b6a49407a3c4b72f78fcaf6cc1
Katelyn <katelyn.thode@gmail.com>
2012-04-09 17:09:01 -0500

    base register is whatever we want it to be

--- arm/FreeRTOS/board/tll6219/regaddr.h
+++ arm/FreeRTOS/board/tll6219/regaddr.h
@@ -2,18 +2,18 @@
 #define REGADDR_H
 
 /* Registers for sensors and LEDs in fpga logic */
-#define SENSREGA 0xd3000000
-#define SENSREGB 0xd3000004
-#define SENSREGC 0xd3000008
-#define SENSREGD 0xd300000c
-#define SENSREGE 0xd3000010
-#define SENSREGF 0xd3000014
-#define SENSREGG 0xd3000018
-#define LED1REG  0xd300001c
-#define LED2REG  0xd3000020
-#define LED3REG  0xd3000024
+#define SENSREGA    0xd3000000
+#define SENSREGB    0xd3000004
+#define SENSREGC    0xd3000008
+#define SENSREGD    0xd300000c
+#define SENSREGE    0xd3000010
+#define SENSREGF    0xd3000014
+#define SENSREGG    0xd3000018
+#define LEDOCT3REG  0xd300001c
+#define LEDOCT4REG  0xd3000020
+#define LEDOCT5REG  0xd3000024
 
-// TODO base address for Audio codec
+// base address for Audio codec
 #define AUDIO_BASE 		0xd3000028
 
 // offsets for Audio codec
