df39d6962740454a89f2a7620454279ac8f45c0f
Katelyn <katelyn.thode@gmail.com>
2012-04-12 14:47:32 -0500

    created look up table for frequencies, defined note frequencies and cases, defined uart LED output values

--- arm/FreeRTOS/board/tll6219/main.c
+++ arm/FreeRTOS/board/tll6219/main.c
@@ -32,8 +32,310 @@
 #include <stdio.h>
 #include <uart.h>
 #include "regaddr.h"
+#include "noteFreq.h"
+
+
+// Notes
+
+#define O3BS	0
+#define O3C	1	
+#define O3CS	3
+#define O3DF	4
+#define O3D	5
+#define O3DS	6
+#define O3EF	7
+#define O3E	8
+#define O3FF	9
+#define O3F	10
+#define O3FS	11
+#define O3GF	12
+#define O3G	13
+#define O3GS	14
+#define O3AF	15
+#define O3A	16
+#define O3AS	17
+#define O3BF	18
+#define O3B	19
+#define O3CF	20
+#define O4BS    21
+#define O4C     22     
+#define O4CS    23
+#define O4DF    24
+#define O4D     25
+#define O4DS    26
+#define O4EF    27
+#define O4E     28
+#define O4FF    29
+#define O4F     30
+#define O4FS    31
+#define O4GF    32
+#define O4G     33
+#define O4GS    34
+#define O4AF    35
+#define O4A     36
+#define O4AS    37
+#define O4BF    38
+#define O4B     39
+#define O4CF    40
+#define O5BS    41
+#define O5C     42      
+#define O5CS    43
+#define O5DF    44
+#define O5D     45
+#define O5DS    46
+#define O5EF    47
+#define O5E     48
+#define O5FF    49
+#define O5F     50
+#define O5FS    51
+#define O5GF    52
+#define O5G     53
+#define O5GS    54
+#define O5AF    55
+#define O5A     56
+#define O5AS    57
+#define O5BF    58
+#define O5B     59
+#define O5CF    60
+
+// UART Defines
+#define UC 0b1000000000000
+#define UD 0b0100000000000
+#define UE 0b0010000000000
+#define UF 0b0001000000000
+#define UG 0b0000100000000
+#define UA 0b0000010000000
+#define UB 0b0000001000000
+#define UL3 0b0000000100000
+#define UR3 0b0000000010000
+#define UL4 0b0000000001000
+#define UR4 0b0000000000100
+#define UL5 0b0000000000010
+#define UR5 0b0000000000001
+
+#define SINE_LUT_LEN 17
+int SINE_LUT[SINE_LUT_LEN] = { 0, 6424, 12785, 19024, 25079, 30893, 36409, 41575, 46340, 50659, 54490, 57797, 60546, 62713, 64276, 65219, 65535};
+
+// variables
+int freq;
+
+// <C> <D> <E> <F> <G> <A> <B> <L3> <R3> <L4> <R4> <L5> <R5>
+int uart_out;
+int uart_in;
+
+/***************************************************************************
+ * plays note
+ **************************************************************************/
+void play (int note) 
+{
+	uart_out = 0b0000000000000;
+	switch (note) {
+		//////////////////////
+		// Octave 3
+		////////////////////// 
+		case O3BS :
+			freq = B3SHARP;
+			// B, R3
+			uart_out |= UB;
+			uart_out |= UR3;
+			break;
+		case O3C :
+			freq = C3;
+			// C, L3, R3
+			uart_out |= UC;
+			uart_out |= UL3;
+			uart_out |= UR3;
+			break;
+		case O3CS :
+			freq = C3SHARP;
+			// C, R3
+			uart_out |= UC;
+			uart_out |= UR3;
+			break;
+		case O3DF :
+			freq = D3FLAT;
+			break;
+		case O3D :
+			freq = D3;
+			break;
+		case O3DS :
+			freq = D3SHARP;
+			break;
+		case O3EF :
+			freq = E3FLAT;
+			break;
+		case O3E :
+			freq = E3;
+			break;
+		case O3FF :
+			freq = F3FLAT;
+			break;
+		case O3F :
+			freq = F3;
+			break;
+		case O3FS :
+			freq = F3SHARP;
+			break;	
+		case O3GF :
+			freq = G3FLAT;
+			break;
+		case O3G :
+			freq = G3;
+			break;
+		case O3GS :
+			freq = G3SHARP;
+			break;
+		case O3AF :
+			freq = A3FLAT;
+			break;
+		case O3A :
+			freq = A3FLAT;
+			break;
+		case O3AS :
+			freq = A3SHARP;
+			break;
+		case O3BF :
+			freq = B3FLAT;
+			break;
+		case O3B :
+			freq = B3;
+			break;
+		case O3CF :
+			freq = C3FLAT;
+			break;
+		///////////////////////////////
+		// Octave 4
+		///////////////////////////////
+		case O4BS :
+			freq = B4SHARP;
+			break;
+		case O4C :
+			freq = C4;
+			break;
+		case O4CS :
+			freq = C4SHARP;
+			break;
+		case O4DF :
+			freq = D4FLAT;
+			break;
+		case O4D :
+			freq = D4;
+			break;
+		case O4DS :
+			freq = D4SHARP;
+			break;
+		case O4EF :
+			freq = E4FLAT;
+			break;
+		case O4E :
+			freq = E4;
+			break;
+		case O4FF :
+			freq = F4FLAT;
+			break;
+		case O4F :
+			freq = F4;
+			break;
+		case O4FS :
+			freq = F4SHARP;
+			break;
+		case O4GF :
+			freq = G4FLAT;
+			break;
+		case O4G :
+			freq = G4;
+			break;
+		case O4GS :
+			freq = G4SHARP;
+			break;
+		case O4AF :
+			freq = A4FLAT;
+			break;
+		case O4A :
+			freq = A4;
+			break;
+		case O4AS :
+			freq = A4SHARP;
+			break;
+		case O4BF :
+			freq = B4FLAT;
+			break;
+		case O4B :
+			freq = B4;
+			break;
+		case O4CF : 
+			freq = C4FLAT;
+			break;
+		////////////////////////////////
+		// Octave 5
+		//////////////////////////////// 
+		case O5BS :
+			freq = B5SHARP;
+			break;
+		case O5C :
+			freq = C5;
+			break;
+		case O5CS :
+			freq = C5SHARP;
+			break;
+		case O5DF :
+			freq = D5FLAT;
+			break;
+		case O5D :
+			freq = D5;
+			break;
+		case O5DS :
+			freq = D5SHARP;
+			break;
+		case O5EF :
+			freq = E5FLAT;
+			break;
+		case O5E :
+			freq = E5;
+			break;
+		case O5FF :
+			freq = F5FLAT;
+			break;
+		case O5F :
+			freq = F5;
+			break;
+		case O5FS :
+			freq = F5SHARP;
+			break;
+		case O5GF :
+			freq = G5FLAT;
+			break;
+		case O5G :
+			freq = G5;
+			break;
+		case O5GS :
+			freq = G5SHARP;
+			break;
+		case O5AF :
+			freq = A5FLAT;
+			break;
+		case O5A :
+			freq = A5;
+			break;
+		case O5AS :
+			freq = A5SHARP;
+			break;
+		case O5BF :
+			freq = B5FLAT;
+			break;
+		case O5B :
+			freq = B5;
+			break;
+		case O5CF :
+			freq = C5FLAT;
+			break;	
+	}
+
+}
+
+
 /****************************************************************************
- *
+ * main function
  ****************************************************************************/
 void _main(void* parameter)
 {
---
+++ arm/FreeRTOS/board/tll6219/noteFreq.h
@@ -0,0 +1,68 @@
+#ifndef NOTEFREQ_H
+#define NOTEFREQ_H
+
+// Note Frequencies in kHz
+#define B3SHARP 251
+#define C3 	251
+#define C3SHARP 236
+#define D3FLAT 	236
+#define D3 	223
+#define D3SHARP 211
+#define E3FLAT 	211
+#define E3	199
+#define F3FLAT 	199
+#define F3 	188
+#define F3SHARP 177
+#define G3FLAT 	177
+#define G3 	167
+#define G3SHARP 158
+#define A3FLAT 	158
+#define A3 	149
+#define A3SHARP 141
+#define B3FLAT 	141
+#define B3 	133
+#define C3FLAT 	133
+
+#define B4SHARP 125
+#define C4      125
+#define C4SHARP 118
+#define D4FLAT  118
+#define D4      112
+#define D4SHARP 105
+#define E4FLAT  105
+#define E4      99
+#define F4FLAT  99
+#define F4      94
+#define F4SHARP 89
+#define G4FLAT  89
+#define G4      84
+#define G4SHARP 79
+#define A4FLAT  79
+#define A4      74
+#define A4SHARP 70
+#define B4FLAT  70
+#define B4      66
+#define C4FLAT  66
+
+#define B5SHARP 63
+#define C5      63
+#define C5SHARP 59
+#define D5FLAT  59
+#define D5      56
+#define D5SHARP 53
+#define E5FLAT  53
+#define E5      50
+#define F5FLAT  50
+#define F5      47
+#define F5SHARP 44
+#define G5FLAT  44
+#define G5      42
+#define G5SHARP 39
+#define A5FLAT  39
+#define A5      37
+#define A5SHARP 35
+#define B5FLAT  35
+#define B5      33
+#define C5FLAT  33
+
+#endif
