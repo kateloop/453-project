4df37410f36f009bd35ab5c6cb8a8b8ae5107f0b
Katelyn <katelyn.thode@gmail.com>
2012-04-25 18:52:29 -0500

    added .c file for AC97 config on the arm

---
+++ arm/testProg.c
@@ -0,0 +1,5 @@
+#define SACNT1_VAL 0h00000011	// 48kHz fixed mode
+#define SRCCR1_VAL 0h00007000	// TODO check value - do we even need to set?
+#define SCR1_VAL   0h00000013	// TODO check value - do we even need to set?
+// TODO STCR pg 565
+// pg 596 -- VERY IMPORTANT
